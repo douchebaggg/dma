7f9f00e8d1eeda33058b7fb6447864b9c2195529
Jarudet Sangprapai <49513867+douchebaggg@users.noreply.github.com>
2025-04-19 12:18:42 +0700

    build command

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-302896ff'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.fqrfrsnq1a"
+    "revision": "0.sk863pdujj8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
