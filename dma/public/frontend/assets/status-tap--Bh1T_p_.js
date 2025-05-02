0e6f95fa4abc72d458bbcccd2d5e7de8a9ed2dcc
Jarudet <Jarudet007@gmail.com>
2025-05-02 13:54:27 +0700

    build cmd

--- dma/public/frontend/assets/status-tap--Bh1T_p_.js
+++
@@ -1,4 +0,0 @@
-import{E as i,G as a,H as c,J as d,K as l}from"./index-BmKAoypp.js";import"./frappe-ui-CYJxxotI.js";/*!
- * (C) Ionic http://ionicframework.com - MIT License
- */const w=()=>{const e=window;e.addEventListener("statusTap",()=>{i(()=>{const o=e.innerWidth,s=e.innerHeight,n=document.elementFromPoint(o/2,s/2);if(!n)return;const t=a(n);t&&new Promise(r=>c(t,r)).then(()=>{d(async()=>{t.style.setProperty("--overflow","hidden"),await l(t,300),t.style.removeProperty("--overflow")})})})})};export{w as startStatusTap};
-//# sourceMappingURL=status-tap--Bh1T_p_.js.map
--- frontend/src/utils/checkIP.ts
+++ frontend/src/utils/checkIP.ts
@@ -9,10 +9,6 @@ export function isLocalNetwork() {
            hostname.endsWith('.local'); // Bonjour/mDNS addresses
 }
 export const currentHost = window.location.hostname;
-<<<<<<< HEAD
 export const port = 8000; // Default port for local development
-=======
-export const port = 8000; 
->>>>>>> f35a647 (fix some code and run build cmd)
 export const urlPort = isLocalNetwork() ? `http://${currentHost}:${port}` : import.meta.env.VITE_API_URL
 export const url = isLocalNetwork() ? `http://192.168.10.105` : import.meta.env.VITE_API_URL
