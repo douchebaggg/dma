e6e9f7a73237067b9bb8e5b67947dabc8ba94755
ALZO GROUP <info@alzogroup.co.th>
2025-09-14 15:05:31 +0700

    change padding px

--- frontend/src/components/core/BottomTabs.vue
+++ frontend/src/components/core/BottomTabs.vue
@@ -1,8 +1,8 @@
 <template>
-	<ion-tab-bar slot="bottom" class=" pt-4 shadow-[1px_-3px_6px_0px_rgba(0,_0,_0,_0.1)]">
+	<ion-tab-bar slot="bottom" class=" pt-6 shadow-[1px_-3px_6px_0px_rgba(0,_0,_0,_0.1)]">
 		<div class=" grid grid-cols-4">
 		<ion-tab-button
-			class="rounded-l-md pb- "
+			class="rounded-l-md pb-5 "
 			tab="dashboardTab"
 			href="/tabs/dashboard"
 		>
@@ -15,7 +15,7 @@
 			<p class=" py-1 text-sm">{{ t("nav.dashboard") }}</p>
 		</ion-tab-button>
 		<ion-tab-button
-			class="rounded-r-md mb-4"
+			class="rounded-r-md pb-5"
 			tab="DocTab"
 			href="/tabs/doctype"
 		>
@@ -27,7 +27,7 @@
 		<p class=" py-1 text-sm">{{ t("nav.document") }}</p>
 		</ion-tab-button>
 		<ion-tab-button
-			class="rounded-r-md mb-4"
+			class="rounded-r-md pb-5"
 			tab="QRScanTab"
 			href="/tabs/qrscaner"
 		>
@@ -40,7 +40,7 @@
 			<p class=" py-1 text-sm">QR Scan</p>
 		</ion-tab-button>
 		<ion-tab-button
-			class="rounded-r-md mb-4"
+			class="rounded-r-md pb-5"
 			tab="AccountTab"
 			href="/tabs/account"
 		>
